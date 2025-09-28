6e0ccce519bf63897927675a91bb6dad03d52886
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-09-28 17:22:20 +0500

    i

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var row = 3
 var col = 3
 
+
+//     for (var i = 0; i<row;i++){
+//         var a = ""
+//         for(var j = 0; j<col ;j++){
+//             a = a+"*"
+//         }
+//         console.log(a,"\n")
+// }
+
+
+// var a = ""
+// var b = " "
+//     for (var i = 0; i<row;i++){
+//         for(var j = 0; j<1 ;j++){
+            
+//             a = a +" " + "*"
+//         }
+//         console.log(a)
+// }
+
 //     for (var i = 0; i<2;i++){
 //         var a = ""
 //         for(var j = 0; j<2 ;j++){
@@ -10,14 +30,14 @@ var col = 3
 //         console.log(a,"\n")
 
 
-var b = 0
-for (var i = 0; i < 3; i++) {
-    var a = ""
-    for (var j = 0; j < 3; j++) {
-            a += b+1
-            b++
+// var b = 0
+// for (var i = 0; i < 3; i++) {
+//     var a = ""
+//     for (var j = 0; j < 3; j++) {
+//             a += b+1
+//             b++
         
         
-    }
-    console.log(a) 
-}
+//     }
+//     console.log(a) 
+// }
